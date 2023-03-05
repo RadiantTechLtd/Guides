272bddec36e3bf17c1bcfc4a751c86079f7a674c
FreddyWordingham <freddy@digilab.co.uk>
2023-03-05 22:45:57 +0000

    Added chapter 3:2

---
+++ sections/3_FastAPI_Backend/chapters/2_HTML/api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+
+import mandybrot as mandy
+
+from . import settings
+
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return settings.TEMPLATES.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/sample/{real}/{imag}")
+async def sample_point(real: float, imag: float):
+    max_iters = 100
+    result = mandy.sample.point(real, imag, max_iters)
+    return f"({real}, {imag}) -> {result}"
