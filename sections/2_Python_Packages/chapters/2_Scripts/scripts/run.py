99a396e2e8b6f39b98335308773d36301248a44f
FreddyWordingham <freddy@digilab.co.uk>
2023-03-05 18:28:41 +0000

    Fixed typo

--- sections/2_Python_Packages/chapters/2_Scripts/scripts/run.py
+++ sections/2_Python_Packages/chapters/2_Scripts/scripts/run.py
@@ -6,6 +6,6 @@ parser.add_argument("real", type=float)
 parser.add_argument("imag", type=float)
 args = parser.parse_args()
 
-iterations = mandy.sample(args.real, args.imag)
+iterations = mandy.sample.point(args.real, args.imag)
 
 print(f"({args.real}, {args.imag}) -> {iterations}")
